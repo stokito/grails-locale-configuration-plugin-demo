d75ba3a7e5a806e3e9e035cd1aa33f92eb8ebf2e
Sergey Ponomarev <stokito@gmail.com>
2013-12-18 13:07:25 +0200

    update to grails 2.3.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,16 +44,13 @@ grails.project.dependency.resolution = {
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.42"
+        build ":tomcat:7.0.47"
 
         // plugins for the compile step
         compile ":scaffolding:2.0.1"
-        compile ':cache:1.1.1'
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.2" // or ":hibernate4:4.1.11.2"
-        runtime ":database-migration:1.3.5"
-        runtime ":jquery:1.10.2"
+        runtime ":hibernate:3.6.10.6" // or ":hibernate4:4.1.11.2"
         runtime ":resources:1.2.1"
         //WATCHME
         compile ':locale-configuration:1.0'
